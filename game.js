6d2f53d6a75175d36f7e2b3aee9ffa8cd57a256e
jeremiedr <jeremiedrouinl@gmail.com>
2025-08-27 16:36:09 -0400

    Update game.js

--- game.js
+++ game.js
@@ -95,7 +95,7 @@ let level4 = [
   'DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD',
 ];
 
-let levels = [level1, level2, level3, level4];
+let levels = [level2, level1, level3, level4];
 
  // let levelImages = null;
 
